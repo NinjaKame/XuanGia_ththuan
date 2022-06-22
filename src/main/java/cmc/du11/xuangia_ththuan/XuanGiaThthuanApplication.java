ba6f6c3031097632dedfcfc95b19b43e921995fe
Thanos learn to code <49831752+NinjaKame@users.noreply.github.com>
2022-06-23 00:53:46 +0700

    First commit

---
+++ src/main/java/cmc/du11/xuangia_ththuan/XuanGiaThthuanApplication.java
@@ -0,0 +1,13 @@
+package cmc.du11.xuangia_ththuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuanGiaThthuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuanGiaThthuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmc/du11/xuangia_ththuan/XuanGiaThthuanApplicationTests.java
@@ -0,0 +1,13 @@
+package cmc.du11.xuangia_ththuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuanGiaThthuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
